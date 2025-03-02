ad2ac41d1051c499959c726f2692b651227ed713
Hiroyuki_Igusa_mac11 <igusahiroyuki@igusanoMacBook-Air.local>
2025-03-03 08:01:14 +0900

    port updated

--- app.py
+++ app.py
@@ -318,4 +318,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # PORT環境変数を使用、無ければ5000
+    app.run(host="0.0.0.0", port=port, debug=False)  # 0.0.0.0で外部アクセスを許可
