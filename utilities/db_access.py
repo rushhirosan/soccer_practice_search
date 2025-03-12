7592e11392bddc7f8a9640dfc455f6ad6d4c4dc3
Hiroyuki_Igusa_mac11 <igusahiroyuki@igusanoMacBook-Air.local>
2025-03-13 07:50:05 +0900

    host updated

--- utilities/db_access.py
+++ utilities/db_access.py
@@ -25,7 +25,7 @@ DATABASE_CONFIG = {
     'dbname': os.getenv('DB_NAME'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST', 'localhost'),
+    'host': os.getenv('DB_HOST'),
     'port': os.getenv('DB_PORT', '5432'),
 }
 
